8a337a3ca4209bf8695ca21d687729ce8fb44ad2
Welson <welsonlimawlsn@gmail.com>
2019-06-30 19:52:49 -0300

    AbstractEntity

---
+++ src/main/java/com/meucontrole/api/entities/AbstractEntity.java
@@ -0,0 +1,34 @@
+package com.meucontrole.api.entities;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public class AbstractEntity implements Serializable {
+
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
