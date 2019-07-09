b271da79e96c474b11e4bc063e728cb98769c4f0
Welson Teles <welsonlimawlsn@gmail.com>
2019-07-09 11:31:34 -0300

    Mapeamento BD

--- src/main/java/com/meucontrole/api/entities/AccountBlock.java
+++ src/main/java/com/meucontrole/api/entities/AccountBlock.java
@@ -3,15 +3,21 @@ package com.meucontrole.api.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tbl_account_block")
 public class AccountBlock extends AbstractEntity {
 
+    @Column(name = "cln_email", nullable = false)
     private String email;
+
+    @Column(name = "cln_expiration", nullable = false)
     private LocalDateTime expiration;
 
 }
